18dbee34a242a9f70df33391b74c59daa78f3e54
Patrick Abraham <patrickabraham@Patricks-MacBook-Air.local>
2026-08-20 16:02:17 +0530

    feat(android): wire MainActivity to Razorpay's PaymentResultWithDataListener

--- android/app/src/main/java/com/greenflagapp/app/MainActivity.java
+++ android/app/src/main/java/com/greenflagapp/app/MainActivity.java
@@ -1,5 +1,32 @@
 package com.greenflagapp.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.razorpay.PaymentData;
+import com.razorpay.PaymentResultWithDataListener;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity implements PaymentResultWithDataListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(RazorpayCheckoutPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onPaymentSuccess(String razorpayPaymentId, PaymentData paymentData) {
+        RazorpayCheckoutPlugin plugin = getRazorpayCheckoutPlugin();
+        if (plugin != null) plugin.handlePaymentSuccess(razorpayPaymentId, paymentData);
+    }
+
+    @Override
+    public void onPaymentError(int code, String description, PaymentData paymentData) {
+        RazorpayCheckoutPlugin plugin = getRazorpayCheckoutPlugin();
+        if (plugin != null) plugin.handlePaymentError(code, description);
+    }
+
+    private RazorpayCheckoutPlugin getRazorpayCheckoutPlugin() {
+        if (getBridge() == null || getBridge().getPlugin("RazorpayCheckout") == null) return null;
+        return (RazorpayCheckoutPlugin) getBridge().getPlugin("RazorpayCheckout").getInstance();
+    }
+}
